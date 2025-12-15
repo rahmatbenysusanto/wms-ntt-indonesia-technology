efa5b641582cc3b24885320c8985d2730c8b1967
Rahmat Beny Susanto <rahmat.beny12@gmail.com>
2025-12-15 13:25:31 +0700

    update

--- resources/views/dashboard-customer/index.blade.php
+++ resources/views/dashboard-customer/index.blade.php
@@ -114,7 +114,7 @@
                         <div class="card-body">
                             <div class="d-flex align-items-center">
                                 <div class="flex-grow-1 overflow-hidden">
-                                    <p class="text-uppercase fw-medium text-muted text-truncate mb-0">Total Price</p>
+                                    <p class="text-uppercase fw-medium text-muted text-truncate mb-0">Total Value</p>
                                 </div>
                             </div>
                             <div class="d-flex align-items-end justify-content-between mt-4">
